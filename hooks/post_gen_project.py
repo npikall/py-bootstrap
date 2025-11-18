19686e5b155fe37d33a0eb84384546b62b0339e9
npikall <nikolas.pikall@gmail.com>
2025-11-18 18:54:57 +0100

    revert: update python in template

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,34 +1,11 @@
 # ruff: noqa: D100, D101, D103 INP001
 import shutil
-from datetime import date
 from enum import StrEnum, auto
 from pathlib import Path
 
-import httpx
-from packaging.version import parse
-from pydantic import BaseModel, ConfigDict, TypeAdapter
 from rich import print  # noqa: A004
 
 PROJECT_DIRECTORY = Path.cwd().resolve()
-
-
-class EndOfLifeResponse(BaseModel):
-    model_config = ConfigDict(extra="ignore")
-
-    cycle: str
-    releaseDate: date  # noqa: N815
-    eol: date
-
-
-eol_list_adapter = TypeAdapter(list[EndOfLifeResponse])
-
-
-def _request_python_versions() -> list[EndOfLifeResponse]:
-    url = "https://endoflife.date/api/python.json"
-    resp = httpx.get(url)
-    resp.raise_for_status()
-    data = resp.json()
-    return eol_list_adapter.validate_python(data)
 
 
 class SupportedCI(StrEnum):
@@ -50,48 +27,6 @@ def remove_file(file: str) -> None:
 def remove_dir(directory: str) -> None:
     dir_path: Path = PROJECT_DIRECTORY / directory
     shutil.rmtree(dir_path)
-
-
-def update_python() -> None:
-    """Update Python versions in Template.
-
-    To be run manually.
-    """
-    m: list[EndOfLifeResponse] = _request_python_versions()
-    m.sort(key=lambda x: parse(x.cycle), reverse=True)
-    future_version = m[0].cycle
-    current_version = m[1].cycle
-    previous_versions_1 = m[2].cycle
-    previous_versions_2 = m[3].cycle
-    previous_versions_3 = m[4].cycle
-
-    re_mapping: dict[str, str] = {
-        "[PY_VERSION_F]": future_version,
-        "[PY_VERSION_C]": current_version,
-        "[PY_VERSION_1]": previous_versions_1,
-        "[PY_VERSION_2]": previous_versions_2,
-        "[PY_VERSION_3]": previous_versions_3,
-    }
-
-    files_to_update: list[str] = [
-        "Justfile",
-        ".python-version",
-        "pyproject.toml",
-        ".gitlab-ci.yml",
-        ".github/workflows/run_tests.yml",
-    ]
-
-    for file in files_to_update:
-        filepath = PROJECT_DIRECTORY / file
-        old_contents = filepath.read_text()
-        new_contents = old_contents
-
-        for old, new in re_mapping.items():
-            new_contents = new_contents.replace(old, new)
-
-        with filepath.open("w", encoding="utf-8") as f:
-            f.write(new_contents)
-        print(f"Updated: {filepath}")
 
 
 def main() -> None:
@@ -125,8 +60,6 @@ def main() -> None:
     if has_ci_code_quality == PromptBool.NO:
         remove_file(".github/workflows/code_quality.yml")
         remove_file(".github/actions/setup/action.yml")
-
-    update_python()
 
     print(":rocket: [green bold] Project successfully initialized.")
 
