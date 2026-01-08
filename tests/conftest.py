0e8d881f13ff730e8db57c693e80f9634aa21d95
Nikolas Pikall <nikolas.pikall@gmail.com>
2026-01-08 18:25:22 +0100

    test: add TypeHints to UserAnswer Model

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,10 @@
+from typing import Literal
+
 from pydantic import BaseModel
+
+type CopyrightLicenseOptions = Literal["Apache-2.0", "MIT", "Unlicense", "None"]
+type DocsEngineOptions = Literal["zensical", "mkdocs"]
+type PreCommitOptions = Literal["pre-commit", "prek"]
 
 
 class BaseUserAnswers(BaseModel):
@@ -10,14 +16,14 @@ class BaseUserAnswers(BaseModel):
     ci_gitlab: bool = True
     repo_name: str = "example"
     include_docs: bool = True
-    docs_engine: str = "zensical"
-    pre_commit: str = "prek"
+    docs_engine: DocsEngineOptions = "zensical"
+    pre_commit: PreCommitOptions = "prek"
     use_goreleaser: bool = True
 
 
 class FullUserAnswers(BaseUserAnswers):
-    copyright_license: str = "MIT"
+    copyright_license: CopyrightLicenseOptions = "MIT"
 
 
 class ChooseLicense(BaseUserAnswers):
-    copyright_license: str
+    copyright_license: CopyrightLicenseOptions
