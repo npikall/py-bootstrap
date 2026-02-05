cc5c27c7670d286b32b801e23864795641f8c5f6
npikall <nikolas.pikall@gmail.com>
2026-02-05 14:38:32 +0100

    test: update justfile lines

--- tests/test_copier.py
+++ tests/test_copier.py
@@ -131,7 +131,7 @@ def test_justfile_renders_correct(session_tmp_path: Path):
         # Want Rendering
         {"want": f"uv run --python={PY_VERSION} pytest", "start": 40, "end": 42},
         {"want": "\nhooks:\n    uvx prek install\n\n#", "start": 53, "end": 58},
-        {"want": "    uv run zensical serve\n\n# initialize", "start": 106, "end": 110},
+        {"want": "    uv run zensical serve\n\n# initialize", "start": 108, "end": 114},
         # No Rendering
         {"want": r"uv run --python={{ PYTHON }} ruff format .", "start": 47, "end": 49},
     ]
