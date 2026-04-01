7e997221322e84e655a6fd4e9a5a7aeba53085d6
npikall <nikolas.pikall@gmail.com>
2026-04-01 16:17:25 +0200

    feat: add updating functionality to script

--- scripts/update_template_dependencies.py
+++ scripts/update_template_dependencies.py
@@ -1,45 +1,271 @@
-import tomllib
+import logging
+import re
+from dataclasses import dataclass
+from enum import Enum, auto
+from functools import lru_cache
 from pathlib import Path
 
-from pydantic import BaseModel, ConfigDict, Field
+import httpx
+
+log = logging.getLogger(__name__)
 
 
-class GitRepoError(Exception):
-    """Base Exception for Git Repo Path."""
+class Resolver(Enum):
+    DEFAULT = auto()
+    UPPER_BOUND = auto()
+    GITHUB_ACTION = auto()
 
 
-def main():
+@dataclass(frozen=True)
+class DependencyRule:
+    file_glob: str
+    package: str
+    pattern: str
+    resolver: Resolver
+
+
+def pattern_greater_equal_semver(package: str) -> str:
+    return rf"\"{package}>=([0-9]*.[0-9]*.[0-9]*)\""
+
+
+RULES = [
+    DependencyRule(
+        file_glob="**/pyproject.toml.jinja",
+        package="pytest",
+        pattern=pattern_greater_equal_semver("pytest"),
+        resolver=Resolver.DEFAULT,
+    ),
+    # TODO: missing PyTest-cov  # noqa: FIX002, TD002, TD003
+    DependencyRule(
+        file_glob="**/pyproject.toml.jinja",
+        package="ruff",
+        pattern=pattern_greater_equal_semver("ruff"),
+        resolver=Resolver.DEFAULT,
+    ),
+    DependencyRule(
+        file_glob="**/pyproject.toml.jinja",
+        package="ty",
+        pattern=pattern_greater_equal_semver("ty"),
+        resolver=Resolver.DEFAULT,
+    ),
+    DependencyRule(
+        file_glob="**/pyproject.toml.jinja",
+        package="mkdocstrings-python",
+        pattern=pattern_greater_equal_semver("mkdocstrings-python"),
+        resolver=Resolver.DEFAULT,
+    ),
+    DependencyRule(
+        file_glob="**/pyproject.toml.jinja",
+        package="zensical",
+        pattern=pattern_greater_equal_semver("zensical"),
+        resolver=Resolver.DEFAULT,
+    ),
+    DependencyRule(
+        file_glob="**/pyproject.toml.jinja",
+        package="uv-build",
+        pattern=r"\"uv-build>=([0-9]*.[0-9]*.[0-9]*),<[0-9]*.[0-9]*\"",
+        resolver=Resolver.UPPER_BOUND,
+    ),
+    DependencyRule(
+        file_glob="**/*.github*/workflows/*docs.yml*",
+        package="actions/configure-pages",
+        pattern=r"actions/configure-pages@v[0-9]*",
+        resolver=Resolver.GITHUB_ACTION,
+    ),
+    DependencyRule(
+        file_glob="**/*.github*/workflows/*.yml*.jinja",
+        package="actions/checkout",
+        pattern=r"actions/checkout@v[0-9]*",
+        resolver=Resolver.GITHUB_ACTION,
+    ),
+    DependencyRule(
+        file_glob="**/*.github*/workflows/*docs.yml*",
+        package="actions/setup-python",
+        pattern=r"actions/setup-python@v[0-9]*",
+        resolver=Resolver.GITHUB_ACTION,
+    ),
+    DependencyRule(
+        file_glob="**/*.github*/workflows/*docs.yml*",
+        package="actions/upload-pages-artifact",
+        pattern=r"actions/upload-pages-artifact@v[0-9]*",
+        resolver=Resolver.GITHUB_ACTION,
+    ),
+    DependencyRule(
+        file_glob="**/*.github*/workflows/*docs.yml*",
+        package="actions/deploy-pages",
+        pattern=r"actions/deploy-pages@v[0-9]*",
+        resolver=Resolver.GITHUB_ACTION,
+    ),
+    DependencyRule(
+        file_glob="**/*.github*/**/*.yml*",
+        package="astral-sh/setup-uv",
+        pattern=r"astral-sh/setup-uv@v[0-9]*",
+        resolver=Resolver.GITHUB_ACTION,
+    ),
+    DependencyRule(
+        file_glob="**/*.github*/workflows/*test_coverage.yml*",
+        package="py-cov-action/python-coverage-comment-action@v3",
+        pattern=r"py-cov-action/python-coverage-comment-action@v[0-9]*",
+        resolver=Resolver.GITHUB_ACTION,
+    ),
+    DependencyRule(
+        file_glob="**/*.github*/actions/setup/action.yml",
+        package="astral-sh/uv",
+        pattern=r"version: \"([0-9]*.[0-9]*.[0-9]*)\"",
+        resolver=Resolver.GITHUB_ACTION,
+    ),
+]
+
+
+def main() -> None:
+    setup_logging()
     root = get_project_root()
-    template_dir = root / "template"
-    process_pyproject_dependencies(template_dir)
+    for rule in RULES:
+        process_rule(rule, root / "template")
 
 
-def process_pyproject_dependencies(template_dir: Path) -> None:
-    file = template_dir / "pyproject.toml"
-    with file.open("rb") as f:
-        data = tomllib.load(f)
-    print(PyprojectModel.model_validate(data))
-    # raise NotImplementedError
+def process_rule(rule: DependencyRule, root: Path) -> None:
+    files = root.rglob(rule.file_glob)
+    for file in files:
+        process_file(rule, file)
 
 
-def get_project_root() -> Path:
+def process_file(rule: DependencyRule, file: Path) -> None:
+    log.debug("process file: %s, exists: %s", file, file.is_file())
+    log.debug("using %s for %s", rule.resolver, rule.package)
+    match rule.resolver:
+        case Resolver.DEFAULT:
+            resolve_default(rule, file)
+        case Resolver.UPPER_BOUND:
+            resolve_upper_bound(rule, file)
+        case Resolver.GITHUB_ACTION:
+            resolve_github_action(rule, file)
+
+
+def resolve_default(rule: DependencyRule, file: Path) -> None:
+    pattern = re.compile(rule.pattern)
+    current = get_current_version(pattern, file.read_text())
+    if current is None:
+        return
+    latest = get_latest_version_from_pypi(rule.package)
+    if latest is None:
+        return
+    if current != latest:
+        log.info("update %s in %s: %s -> %s", rule.package, file.name, current, latest)
+        package_declaration = get_dependency_string(rule.package, latest)
+        update_file(file, pattern, package_declaration)
+    else:
+        log.info("package %s already at latest version", rule.package)
+
+
+def resolve_upper_bound(rule: DependencyRule, file: Path) -> None:
+    pattern = re.compile(rule.pattern)
+    current = get_current_version(pattern, file.read_text())
+    if current is None:
+        return
+    latest = get_latest_version_from_pypi(rule.package)
+    if latest is None:
+        return
+    if current != latest:
+        log.info("update %s in %s: %s -> %s", rule.package, file.name, current, latest)
+        upper = get_next_minor_version(latest)
+        package_declaration = get_dependency_string_with_upper(
+            rule.package, latest, upper
+        )
+        update_file(file, pattern, package_declaration)
+    else:
+        log.info("package %s already at latest version", rule.package)
+
+
+def resolve_github_action(rule: DependencyRule, file: Path) -> None:
+    pattern = re.compile(rule.pattern)
+    current = get_current_version(pattern, file.read_text())
+    if current is None:
+        return
+    latest = get_latest_version_from_github(rule.package)
+    if latest is None:
+        return
+    latest_major = latest.split(".")[0].lstrip("v")
+    if current != latest_major:
+        log.info(
+            "update %s in %s: %s -> %s", rule.package, file.name, current, latest_major
+        )
+        package_declaration = f"{rule.package}@v{latest_major}"
+        update_file(file, pattern, package_declaration)
+    else:
+        log.info("package %s already at latest_major version", rule.package)
+
+
+def update_file(file: Path, pattern: re.Pattern, repl: str) -> None:
+    new_content = update_file_content(file, pattern, repl)
+    file.write_text(new_content)
+
+
+@lru_cache(maxsize=128)
+def get_next_minor_version(version: str) -> str:
+    parts = version.split(".")
+    minor = str(int(parts[1]) + 1)
+    return f"{parts[0]}.{minor}"
+
+
+@lru_cache(maxsize=128)
+def get_dependency_string(package: str, version: str) -> str:
+    return f'"{package}>={version}"'
+
+
+def get_dependency_string_with_upper(package: str, version: str, upper: str) -> str:
+    return f'"{package}>={version},<{upper}"'
+
+
+def update_file_content(file: Path, pattern: re.Pattern[str], repl: str) -> str:
+    return pattern.sub(repl, file.read_text())
+
+
+def get_current_version(pattern: re.Pattern, content: str) -> str | None:
+    matched_stmts = pattern.findall(content)
+    if not matched_stmts:
+        log.warning("no match found for pattern: %s", pattern)
+        return None
+    if len(matched_stmts) > 1:
+        log.warning("multiple matches found: %s", matched_stmts)
+    sorted_versions: list[str] = sorted(matched_stmts, reverse=True)
+    return sorted_versions[0]
+
+
+@lru_cache(maxsize=128)
+def get_latest_version_from_pypi(package: str) -> str | None:
+    resp = httpx.get(f"https://pypi.org/pypi/{package}/json", timeout=10)
+    if resp.status_code != httpx.codes.OK:
+        return None
+    return resp.json().get("info", {}).get("version")
+
+
+@lru_cache(maxsize=128)
+def get_latest_version_from_github(repo: str) -> str | None:
+    resp = httpx.get(f"https://api.github.com/repos/{repo}/releases/latest", timeout=10)
+    if resp.status_code != httpx.codes.OK:
+        return None
+    return resp.json().get("tag_name", "")
+
+
+def setup_logging() -> None:
+    logging.basicConfig(
+        format="{asctime} {levelname} {message}",
+        style="{",
+        datefmt="%Y/%m/%d %H:%M:%S",
+        level=logging.WARNING,
+    )
+    log.setLevel(logging.INFO)
+
+
+def get_project_root(root_marker_file: str = "pyproject.toml") -> Path:
     current_filepath = Path(__file__)
-    root_marker_file = "pyproject.toml"
     for parent in current_filepath.parents:
         if (parent / root_marker_file).exists():
+            log.debug("root: %s", parent)
             return parent
     err = f"no '{root_marker_file}' found in any parent directories"
     raise FileNotFoundError(err)
-
-
-class BuildSystem(BaseModel):
-    model_config = ConfigDict(extra="ignore")
-    requires: list[str]
-
-
-class PyprojectModel(BaseModel):
-    model_config = ConfigDict(extra="ignore")
-    build_system: BuildSystem = Field(alias="build-system")
 
 
 if __name__ == "__main__":
