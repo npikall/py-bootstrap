7258e54b2287783c85b9b17da15e618b3b20bc3e
Nikolas Pikall <nikolas.pikall@gmail.com>
2026-05-16 22:15:50 +0200

    feat: prefill project name with target directory

--- extensions.py
+++ extensions.py
@@ -1,8 +1,22 @@
 import re
 import subprocess
+import sys
 import unicodedata
 
 from jinja2.ext import Extension
+
+
+def read_target_from_args() -> str:
+    args = sys.argv
+    last = args[-1]
+    before_last = args[-2]
+    if not is_option(before_last) and not is_option(last):
+        return last
+    return ""
+
+
+def is_option(arg: str) -> bool:
+    return arg.startswith("-")
 
 
 def git_user_name(default: str) -> str:
@@ -30,6 +44,7 @@ class GitExtension(Extension):
         super().__init__(environment)
         environment.filters["git_user_name"] = git_user_name
         environment.filters["git_user_email"] = git_user_email
+        environment.globals["read_target_from_args"] = read_target_from_args
 
 
 class SlugifyExtension(Extension):
